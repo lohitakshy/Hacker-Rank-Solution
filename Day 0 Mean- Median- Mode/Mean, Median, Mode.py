000bb6494f19768e23371f8fabcf7191830d9aef
Lohitaksh yogi <lohitakshyogi@gmail.com>
2018-10-04 14:55:58 -0400

    Update Mean, Median, Mode.py

--- Day 0 Mean- Median- Mode/Mean, Median, Mode.py
+++ Day 0 Mean- Median- Mode/Mean, Median, Mode.py
@@ -22,3 +22,14 @@ if len(index) == 1:
     print(index)
 else:
     print(min(index))
+    
+    
+    -------------------------------------------------------
+import numpy as np
+from scipy import stats
+
+size = int(input())
+numbers = list(map(int, input().split()))
+print(np.mean(numbers))
+print(np.median(numbers))
+print(int(stats.mode(numbers)[0]))
